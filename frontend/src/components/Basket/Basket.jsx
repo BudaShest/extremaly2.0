b55239aa89a91d2ad998e0cd959292b35ad90f8e
budashest <budashest@gmail.com>
2022-06-24 23:46:51 +0500

    save

--- frontend/src/components/Basket/Basket.jsx
+++ frontend/src/components/Basket/Basket.jsx
@@ -23,7 +23,7 @@ const Basket = ({applications}) => {
             {/*todo url*/}
             <form style={{margin: '40px 0'}} method="post" action="http://extremly.ru:8000/application/create-application">
                 <Row>
-                    <Col s={12} l={9}>
+                    <Col s={12} m={9}>
                         <Row style={{backgroundColor: 'rgba(0, 0, 0, 0.5)'}}>
                             <Col className="white-text " s={6} l={3}  style={{fontSize: '1.3em'}}>Событие:</Col>
                             <Col className="white-text " s={6} l={3} style={{fontSize: '1.3em'}}>Цена билета:</Col>
@@ -35,7 +35,7 @@ const Basket = ({applications}) => {
                                                                         item={application}/>)
                         }
                     </Col>
-                    <Col className={style.summaryBlock} s={12} l={9}>
+                    <Col className={style.summaryBlock} s={12} m={9}>
                         <h4 className="white-text">Итого:</h4>
                         <span className={style.totalSpan}>{totalValue} руб.</span>
                         <Button className={style.submitForm} large>Подтвердить</Button>
